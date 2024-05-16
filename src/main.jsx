e238d795c9490be3942f0828035502e95cd312b7
frosti <extendcrypt01@gmail.com>
2024-05-16 12:11:56 -0300

    google analytics measure

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import { BrowserRouter } from "react-router-dom";
 import ReactGA from "react-ga4";
 
-ReactGA.initialize(import.meta.env.VITE_MEASUREMENT_ID);
+ReactGA.initialize("G-1FHVQRKH8Z");
 ReactGA.send({ hitType: "pageview", page: window.location.pathname });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
