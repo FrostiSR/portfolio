45b614482506156bd87e522c8c037529b631901a
frosti <extendcrypt01@gmail.com>
2024-05-16 12:10:21 -0300

    google analytics

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,10 @@ import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { BrowserRouter } from "react-router-dom";
+import ReactGA from "react-ga4";
+
+ReactGA.initialize(import.meta.env.VITE_MEASUREMENT_ID);
+ReactGA.send({ hitType: "pageview", page: window.location.pathname });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
