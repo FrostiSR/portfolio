7d84232c84cf8dfb19281241b238966b132d7dd1
Daniel Dos Ramos <extendcrypt01@gmail.com>
2025-11-13 14:08:10 +0100

    about me data

--- src/data.js
+++ src/data.js
@@ -7,12 +7,12 @@ export const ABOUT = {
         left: {
             title: 'I am Daniel dos Ramos, a Full-Stack Developer + Graphic Designer',
             content:
-            `I have 3+ years of experience with all phases of the
-            development cycle for dynamic web projects and graphic design. Well
-            versed in numerous programming languages including HTML5, PHP,
-            NodeJS, ReactJS, JavaScript, jQuery, CSS, Bootstrap, MySQL,
-            PostgreSQL and MongoDB. Furthermore, I am someone who enjoys his
-            work and tackles every project with both hands.`
+            `I’m Daniel dos Ramos, born and raised in Suriname. I’ve always had a strong passion for technology and creativity. My IT journey began at Natin-MBO, where I graduated as the top ICT Application Developer. That’s where I discovered not only how systems work, but also how important it is how people experience digital products. This insight gradually steered me toward design and interactive media.
+            In 2020, I received a scholarship to study Information Security in China. I completed a year of Chinese, which I surprisingly enjoyed, but due to COVID-19 I couldn’t travel abroad, and the online classes didn’t begin until late at night because of the time difference. Over time, I realized that the field no longer aligned with my interests, so I decided to stop.
+            The years that followed allowed me to work, save, and learn more about myself. During this period, I gained 3+ years of experience across all phases of the development cycle for dynamic web projects and graphic design. I became well versed in various programming languages and technologies including HTML5, PHP, Node.js, React.js, JavaScript, jQuery, CSS, Bootstrap, MySQL, PostgreSQL, and MongoDB. I’m someone who genuinely enjoys his work and approaches every project with full dedication.
+            In Suriname, academic opportunities are limited, and I had always dreamed of studying abroad to grow both personally and professionally. Eventually, I realized that my true passion lies in combining technology, creativity, and user experience. That’s why I am now fully committed to studying Communication and Multimedia Design in the Netherlands.
+            I am eager to learn, motivated, and ready to grow in this field. My goal is to become a successful UX/UI designer or front-end developer who creates digital experiences that truly help and inspire people.
+            `
         },
         right: {
             name: 'Daniël Dos Ramos',
