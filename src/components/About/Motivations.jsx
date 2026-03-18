6475cbf8c23e2e3204696012f918818d1165dcf0
Daniel Dos Ramos <extendcrypt01@gmail.com>
2026-03-18 13:19:53 +0100

    title motivations

--- src/components/About/Motivations.jsx
+++ src/components/About/Motivations.jsx
@@ -6,7 +6,7 @@ import "./About-Services.css";
 export default function About() {
     return (
         <div>
-            <Header title={"Strengths"} subTitle={"What I bring to a project"} />
+            <Header title={"MOTIVATION"} subTitle={"What drives me"} />
 
             <div className="row g-4 row-cols-1 row-cols-md-2 row-cols-lg-3 justify-content-center">
                 <Col xs={12} md={6} className="text-start p-2">
