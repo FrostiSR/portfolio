decc58d63630c45e38d8a8ba196a2414f1478b22
Daniel Dos Ramos <extendcrypt01@gmail.com>
2026-03-18 12:58:44 +0100

    motivations

--- src/data.js
+++ src/data.js
@@ -1,41 +1,55 @@
-import CvEN from './assets/docs/CV_DosRamosD_2025_EN.pdf';
-import CvNL from './assets/docs/CV_DosRamosD_2025_NL.pdf';
+import CvEN from "./assets/docs/CV_DosRamosD_2025_EN.pdf";
+import CvNL from "./assets/docs/CV_DosRamosD_2025_NL.pdf";
 
 export const ABOUT = {
-  
-    about:{
-        left: {
-            title: 'I am Daniel dos Ramos, a Full-Stack Developer + Designer',
-            content:
-          'Born and raised in <b>Suriname</b>. As a child, I was <b>always fascinated by computers</b>, especially the design side and how technology could be shaped to create meaningful experiences. This curiosity led me to study <b>ICT Application Development</b> at Natin-MBO, where I built a strong foundation in web development and graphic design. In 2020 I received a scholarship to study Information Security in China, but due to COVID-19 I could not travel and the classes took place online late at night because of the time difference. During that time I realized my heart was not fully in that direction, mainly because I had not yet discovered what truly motivated me, so I decided to stop and rethink my path. That moment pushed me to reflect on my interests and I discovered that <b>my real passion</b> lies in combining <b>technology</b>,<b> creativity</b> and <b>user experience</b>. After realizing this, I <b>moved to the Netherlands</b>, where I have now been living and started studying <b>Communication and Multimedia Design</b> at Hogeschool Rotterdam. My goal is to <b>grow into a successful UX/UI designer or front-end developer</b> who creates intuitive and meaningful digital experiences that truly help people.'
-        },
-        right: {
-            name: 'Daniël Dos Ramos',
-            email: 'daniel.dosramos08@gmail.com',
-            age: '26',
-            from: 'Rotterdam, Nederland',
-            freelance: 'Available',
-        }
+  about: {
+    left: {
+      title: "I am Daniel dos Ramos, a Full-Stack Developer + Designer",
+      content:
+        "Born and raised in <b>Suriname</b>. As a child, I was <b>always fascinated by computers</b>, especially the design side and how technology could be shaped to create meaningful experiences. This curiosity led me to study <b>ICT Application Development</b> at Natin-MBO, where I built a strong foundation in web development and graphic design. In 2020 I received a scholarship to study Information Security in China, but due to COVID-19 I could not travel and the classes took place online late at night because of the time difference. During that time I realized my heart was not fully in that direction, mainly because I had not yet discovered what truly motivated me, so I decided to stop and rethink my path. That moment pushed me to reflect on my interests and I discovered that <b>my real passion</b> lies in combining <b>technology</b>,<b> creativity</b> and <b>user experience</b>. After realizing this, I <b>moved to the Netherlands</b>, where I have now been living and started studying <b>Communication and Multimedia Design</b> at Hogeschool Rotterdam. My goal is to <b>grow into a successful UX/UI designer or front-end developer</b> who creates intuitive and meaningful digital experiences that truly help people.",
     },
-    services:{
-        graphic:{
-            title: 'Initiative',
-            content:'I’m a CMD designer who doesn’t wait to be told what to do. When I notice a teammate struggling with a tool like Figma, I take the initiative to help by explaining features, sharing tips and thinking along to find solutions. During a school design project, I also took responsibility for setting up and maintaining the project documentation, which helped keep decisions and progress clear for the whole team. For me, taking initiative means taking responsibility, sharing knowledge, and actively contributing to both the design and the team.',
-          },
-        web:{
-            title: 'Perseverance',
-            content:'I’m a designer with strong perseverance. During a school design project, I fell behind due to personal circumstances. Instead of giving up, I pushed myself to catch up and complete the work to the best of my ability. I spent extra time making sure everything was finished properly and met a good standard. In the end, my effort paid off and I successfully completed the project. As I like to say, “Hard work eventually speaks for itself.”',
-          },
-          support:{
-            title: 'Support',
-            content:'I’m an empathetic designer who enjoys helping and supporting others. During a school design project, we worked extensively with Figma and some teammates had little to no experience with the tool. Because I was already familiar with it, I helped by explaining features, guiding them through tasks and supporting them when they got stuck. By collaborating and helping each other, we were able to create and present a well-functioning prototype as our final result.',
-          },
+    right: {
+      name: "Daniël Dos Ramos",
+      email: "daniel.dosramos08@gmail.com",
+      age: "26",
+      from: "Rotterdam, Nederland",
+      freelance: "Available",
     },
-  };
+  },
+  services: {
+    graphic: {
+      title: "Initiative",
+      content:
+        "I’m a CMD designer who doesn’t wait to be told what to do. When I notice a teammate struggling with a tool like Figma, I take the initiative to help by explaining features, sharing tips and thinking along to find solutions. During a school design project, I also took responsibility for setting up and maintaining the project documentation, which helped keep decisions and progress clear for the whole team. For me, taking initiative means taking responsibility, sharing knowledge, and actively contributing to both the design and the team.",
+    },
+    web: {
+      title: "Perseverance",
+      content:
+        "I’m a designer with strong perseverance. During a school design project, I fell behind due to personal circumstances. Instead of giving up, I pushed myself to catch up and complete the work to the best of my ability. I spent extra time making sure everything was finished properly and met a good standard. In the end, my effort paid off and I successfully completed the project. As I like to say, “Hard work eventually speaks for itself.”",
+    },
+    support: {
+      title: "Support",
+      content:
+        "I’m an empathetic designer who enjoys helping and supporting others. During a school design project, we worked extensively with Figma and some teammates had little to no experience with the tool. Because I was already familiar with it, I helped by explaining features, guiding them through tasks and supporting them when they got stuck. By collaborating and helping each other, we were able to create and present a well-functioning prototype as our final result.",
+    },
+  },
+  motivations: {
+    creativity: {
+      title: "Creativity",
+      content:
+        "Creativity is one of my biggest drivers. When I get the chance to design or build something new, I feel a lot of energy and excitement. I enjoy turning ideas into real interfaces and concepts, especially in digital design. I like experimenting, improving existing ideas, and working under pressure, like during hackathons, where I quickly create innovative solutions. The process of inventing and building something meaningful motivates me the most.",
+    },
+    meaning: {
+      title: "Meaning",
+      content:
+        "Meaning is important to me because I enjoy contributing to others and helping people grow. I often support classmates by explaining concepts or helping with tools like Figma. During Design Skills 3, I also helped others with HTML and CSS. Seeing someone understand something because of my help gives me a strong sense of purpose and makes my work more meaningful.",
+    },
+  },
+};
 
-  export const DOCS = {
-    CV: {
-        EN: CvEN,
-        NL: CvNL,
-    }
-  }
+export const DOCS = {
+  CV: {
+    EN: CvEN,
+    NL: CvNL,
+  },
+};
