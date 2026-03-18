d401e19bba2a55a49323034e0deadc7aa6e6ace2
Daniel Dos Ramos <extendcrypt01@gmail.com>
2026-03-18 13:02:31 +0100

    text center motivation

--- src/components/About/Motivations.jsx
+++ src/components/About/Motivations.jsx
@@ -4,43 +4,43 @@ import { ABOUT } from "../../data";
 import "./About-Services.css";
 
 export default function About() {
-  return (
-    <div>
-      <Header title={"Strengths"} subTitle={"What I bring to a project"} />
+    return (
+        <div>
+            <Header title={"Strengths"} subTitle={"What I bring to a project"} />
 
-      <div className="row g-4 py-5 row-cols-1 row-cols-md-2 row-cols-lg-3">
-        <Col xs={12} md={6} className="text-start p-2">
-  <div className="d-flex flex-row align-items-start gap-3 h-100 service-card">
-    <div className="service-box">
-      <i className="bi bi-palette-fill"></i>
-    </div>
-    <div className="flex-grow-1">
-      <h4 className="fw-bold title-text">
-        {ABOUT.motivations.creativity.title}
-      </h4>
-      <p className="description-text">
-        {ABOUT.motivations.creativity.content}
-      </p>
-    </div>
-  </div>
-</Col>
+            <div className="row g-4 py-5 row-cols-1 row-cols-md-2 row-cols-lg-3 text-center">
+                <Col xs={12} md={6} className="text-start p-2">
+                    <div className="d-flex flex-row align-items-start gap-3 h-100 service-card">
+                        <div className="service-box">
+                            <i className="bi bi-palette-fill"></i>
+                        </div>
+                        <div className="flex-grow-1">
+                            <h4 className="fw-bold title-text">
+                                {ABOUT.motivations.creativity.title}
+                            </h4>
+                            <p className="description-text">
+                                {ABOUT.motivations.creativity.content}
+                            </p>
+                        </div>
+                    </div>
+                </Col>
 
-<Col xs={12} md={6} className="text-start p-2">
-  <div className="d-flex flex-row align-items-start gap-3 h-100 service-card">
-    <div className="service-box">
-      <i className="bi bi-heart-fill"></i>
-    </div>
-    <div className="flex-grow-1">
-      <h4 className="fw-bold title-text">
-        {ABOUT.motivations.meaning.title}
-      </h4>
-      <p className="description-text">
-        {ABOUT.motivations.meaning.content}
-      </p>
-    </div>
-  </div>
-</Col>
-      </div>
-    </div>
-  );
+                <Col xs={12} md={6} className="text-start p-2">
+                    <div className="d-flex flex-row align-items-start gap-3 h-100 service-card">
+                        <div className="service-box">
+                            <i className="bi bi-heart-fill"></i>
+                        </div>
+                        <div className="flex-grow-1">
+                            <h4 className="fw-bold title-text">
+                                {ABOUT.motivations.meaning.title}
+                            </h4>
+                            <p className="description-text">
+                                {ABOUT.motivations.meaning.content}
+                            </p>
+                        </div>
+                    </div>
+                </Col>
+            </div>
+        </div>
+    );
 }
