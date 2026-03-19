962d81bc5eba2806c362de4f5527bef15d8974c2
Daniel Dos Ramos <extendcrypt01@gmail.com>
2026-03-19 11:26:45 +0100

    cmd content

--- src/components/Portfolio/CMD.jsx
+++ src/components/Portfolio/CMD.jsx
@@ -21,12 +21,25 @@ export default function Web() {
       <div className="d-flex flex-row flex-wrap justify-content-start text-center">
          <Fragment>
                 <div className="flex-basis-440 me-2">
-                  <CardCase
-                    image={webcase.image}
-                    title={webcase.title}
-                    url={webcase.description}
-                    type={webcase.type}
-                  />
+                   <div>
+        <div className="img-container">
+          <a href="https://www.figma.com/proto/YULRQycJocoWXdwAdhkYY8/v3_high-fidelity?node-id=39-253&t=dJ7DYz1EoJ1Erzvg-1&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=31%3A556&show-proto-sidebar=1" target="_blank">
+            <img
+              className="img-fluid"
+              loading="lazy"
+              src={
+                "https://res.cloudinary.com/dim8i07e6/image/upload/v1773915723/Slob_Kool_Teal_expositie_v2_tskaba.jpg"
+              }
+              alt={alt}
+            />
+            <div className="image-hover">
+              <h4>Depot Beleving Prototype</h4>
+            </div>
+          </a>
+        </div>
+        <h5>{title}</h5>
+        <p className="description-text">{type}</p>
+      </div>
                 </div>
               </Fragment>
       </div>
